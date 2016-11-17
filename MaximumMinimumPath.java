2d23ef35b0b4a8dd2a9d97fb4e9826e7cb816955
LiJinglin918 <lijinglin918@gmail.com>
2016-11-18 00:23:57 +0700

    Update and rename PathMinimumSum.java to MaximumMinimumPath.java

--- MaximumMinimumPath.java
+++ MaximumMinimumPath.java
@@ -12,22 +12,6 @@ return: 5
 */
 
 
-// DP 方法
-int helper(int[][] matrix){
-    int[] result = new int[matrix[0].length];
-    result[0] = matrix[0][0];
-    for(int i=1; i<matrix[0].length; i++){
-        result[i] = Math.min(result[i-1], matrix[0][i]);
-    }
-    for(int i=1; i<matrix.length; i++){
-        result[0] = Math.min(matrix[i][0], result[0]);
-        for(int j=1; j<matrix[0].length; j++){
-            result[j] = (result[j]<matrix[i][j] && result[j-1]<matrix[i][j])?Math.max(result[j-1], result[j]):matrix[i][j];
-            }
-        }
-        return result[result.length-1];
-}
-
 // DFS 方法
 public class MaximumMinimumPath {
     private int min, max, row, col;
@@ -52,3 +36,22 @@ public class MaximumMinimumPath {
         dfsHelper(matrix, min, i + 1, j);
     }
 }
+
+/*=================================================================================================================*/
+
+// DP 方法
+int helper(int[][] matrix){
+    int[] result = new int[matrix[0].length];
+    result[0] = matrix[0][0];
+    for(int i=1; i<matrix[0].length; i++){
+        result[i] = Math.min(result[i-1], matrix[0][i]);
+    }
+    for(int i=1; i<matrix.length; i++){
+        result[0] = Math.min(matrix[i][0], result[0]);
+        for(int j=1; j<matrix[0].length; j++){
+            result[j] = (result[j]<matrix[i][j] && result[j-1]<matrix[i][j])?Math.max(result[j-1], result[j]):matrix[i][j];
+            }
+        }
+        return result[result.length-1];
+}
+
