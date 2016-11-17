06bc29e4579fe9552cfd7395cd817ac41a59ae60
LiJinglin918 <lijinglin918@gmail.com>
2016-11-17 10:28:46 +0700

    Update ArithmeticSequence.java

--- ArithmeticSequence.java
+++ ArithmeticSequence.java
@@ -28,3 +28,27 @@ public static int getLAS(int[] A){
     res += count;
     return res;
 }
+
+
+/*=========================================================================================================*/
+
+public class ArithmeticSlice {
+    public int Solution(int[] array) {
+        if (array == null || array.length < 3)  return 0;
+        int rvalue = 0, gap = array[1] - array[0], length = 2;
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i+1] - array[i] == gap)   length++;
+            else {
+                gap = array[i+1] - array[i];
+                if (length >= 3)
+                    rvalue += (length - 1) * (length - 2) / 2;
+                //防止溢出？
+                if (rvalue > 1000000000)    return -1;
+                length = 2;
+            }
+        }
+        if (length >= 3)
+            rvalue += (length - 1) * (length - 2) / 2;
+        return rvalue > 1000000000 ? -1 : rvalue;
+    }
+}
