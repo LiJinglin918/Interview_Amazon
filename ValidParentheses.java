3643d9dafbdca8a54137504c4ca577f76f110dbf
LiJinglin918 <lijinglin918@gmail.com>
2017-01-28 01:08:38 +0700

    Update ValidParentheses.java

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -22,3 +22,25 @@ public class Solution {
         return stack.isEmpty();
     }
 }
+
+
+/* 给你一个str,里面只有 '('和‘)’,让你数valid pairs一共有多少,如果不是valid就返回-1. 
+(判断是不是valid的parenthesis string，不是的话返回-1，是的话返回valid pair个数，即String.length() / 2 )
+*/
+
+import java.util.Stack;
+
+public class isValid {
+    public boolean isValidParentheses(String s) {
+        if (s == null || s.length() == 0)   return true;
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.empty())  stack.push(s.charAt(i));
+            else if (s.charAt(i) - stack.peek() == 1 || s.charAt(i) - stack.peek() == 2)    stack.pop();
+            else    stack.push(s.charAt(i));
+        }
+        
+        return stack.empty();
+    }
+}
