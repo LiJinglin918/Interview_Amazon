034a01e49eeb784bc5ed5bb3c422a15bedb5228c
LiJinglin918 <lijinglin918@gmail.com>
2017-01-28 01:11:48 +0700

    Update MinimumPathSum.java

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -1,6 +1,9 @@
 /*
 LC的题目是左上角到右下角的path.此题是求解BST的左子数到右子树的最小path. (最小的路径，即左右没有子树的叶子之间的距离)
 */
+
+// Binary search tree minimum sum from root to leaf
+
 public class PathSum {
     public int Solution(TreeNode root) {
         if (root == null)   return 0;
